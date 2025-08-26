08c6bd62268f8a98fbeff7fff7c4e9e4b9a576f8
khalid999devs <khalidahammeduzzal@gmail.com>
2025-08-26 12:37:27 +0600

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="min-h-screen w-full">
       <div className="flex items-center justify-center w-full flex-col min-h-screen">
         <h1 className="text-3xl font-bold text-center w-full">
-          Hello world! Start From here Root Page: src/app/page.tsx
+          Hello world! Start from here Root Page: src/app/page.tsx
         </h1>
         <Landing params="Landing:" />
       </div>
