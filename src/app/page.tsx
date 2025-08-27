df2926465b60d17e10a9b2a54b0800f2c5ad3514
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-08-27 12:09:46 +0600

    hero section completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import HeroCarousel from './components/HeroCarousel';
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <HeroCarousel />
+    </>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,17 @@
 'use client';
-import React from 'react';
 import Link from 'next/link';
+import { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { RxCross2 } from 'react-icons/rx';
 
 export default function Navbar() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   return (
-    <nav className="w-full py-5 bg-[var(--background)] border border-[var(--border)]">
+    <nav className="w-full py-5 bg-background border border-border">
       <div className="container mx-auto flex items-center justify-between px-4 py-3 md:gap-15">
         <div className="text-xl font-bold">Logo</div>
+
+        {/* For mobile view */}
         <div className="md:hidden inline-block">TextLogo</div>
         {/* Desktop Links */}
         <div className="hidden md:flex justify-end flex-wrap gap-4 space-x-4">
@@ -17,7 +19,7 @@ export default function Navbar() {
             <Link
               key={idx}
               href={link.link}
-              className="transition-colors hover:text-[var(--primary)] px-2 py-1 rounded"
+              className="transition-colors hover:text-primary px-2 py-1 rounded"
             >
               {link.text}
             </Link>
@@ -25,7 +27,7 @@ export default function Navbar() {
         </div>
         {/* Hamburger Icon */}
         <button
-          className="md:hidden flex items-center p-2 focus:outline-none hover:text-[var(--primary)]"
+          className="md:hidden flex items-center p-2 focus:outline-none hover:text-primary"
           onClick={() => setOpen(!open)}
           aria-label="Toggle menu"
         >
@@ -35,13 +37,13 @@ export default function Navbar() {
       </div>
       {/* Mobile Menu */}
       {open && (
-        <div className="md:hidden bg-[var(--background)] shadow-lg border-t border-[var(--border)]">
+        <div className="md:hidden bg-background border-t border-border">
           <div className="flex flex-col items-center py-2">
             {links.map((link, idx) => (
               <Link
                 key={idx}
                 href={link.link}
-                className="py-2 w-full text-center hover:text-[var(--primary)] transition-colors rounded"
+                className="py-2 w-full text-center hover:text-primary transition-colors rounded"
                 onClick={() => setOpen(false)}
               >
                 {link.text}
